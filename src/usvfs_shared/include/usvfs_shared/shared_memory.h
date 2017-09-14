78e8b0ac48848e284660cd4e11b16816387a5ac4
Diana <crazykilla15@gmail.com>
2017-09-14 00:34:32 -0400

    WIP Reimplement shared_memory.h?

--- src/usvfs_shared/include/usvfs_shared/shared_memory.h
+++ src/usvfs_shared/include/usvfs_shared/shared_memory.h
@@ -19,3 +19,34 @@ You should have received a copy of the GNU General Public License
 along with usvfs. If not, see <http://www.gnu.org/licenses/>.
 */
 #pragma once
+#include <cstdint>
+#include <scoped_allocator>
+
+namespace usvfs {
+namespace shared {
+
+// template <typename T>
+// using OffsetPtrT = bi::offset_ptr<T, std::int32_t, std::uint64_t>;
+// typedef OffsetPtrT<void> VoidPointerT;
+
+// important: the windows shared memory mechanism, unlike other impelementations
+// automatically removes the SHM object when there are no more "subscribers".
+// MO currently depends on that feature!
+
+// managed_windows_shared_memory apparently doesn't support sharing between
+// 64bit and 32bit processes
+// typedef bi::basic_managed_windows_shared_memory<char, bi::rbtree_best_fit<bi::mutex_family, VoidPointerT, 8>,
+//                                                bi::iset_index>
+//    managed_windows_shared_memory;
+
+// typedef managed_windows_shared_memory SharedMemoryT;
+
+// typedef SharedMemoryT::segment_manager SegmentManagerT;
+// TODO: Specialize SegmentManagerT
+typedef std::scoped_allocator_adaptor<std::allocator<void>> VoidAllocatorT;
+typedef VoidAllocatorT::rebind<char>::other CharAllocatorT;
+
+typedef std::basic_string<char, std::char_traits<char>, CharAllocatorT> StringT;
+
+} // namespace shared
+} // namespace usvfs
