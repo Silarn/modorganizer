706f907183d5522f1d3283055fecfa71b5ddd261
Diana <crazykilla15@gmail.com>
2017-09-13 23:00:14 -0400

    Castrate shmlogger.h, whatevert the fuck that is

--- src/usvfs_shared/include/usvfs_shared/shmlogger.h
+++ src/usvfs_shared/include/usvfs_shared/shmlogger.h
@@ -19,86 +19,73 @@ You should have received a copy of the GNU General Public License
 along with usvfs. If not, see <http://www.gnu.org/licenses/>.
 */
 #pragma once
-
 #include "usvfs_shared/logging.h"
-#include "usvfs_shared/windows_sane.h"
 #include "usvfs_shared/shared_memory.h"
-#include <boost/interprocess/ipc/message_queue.hpp>
-#include <boost/format.hpp>
+#include "usvfs_shared/windows_sane.h"
 #include <atomic>
-#include <spdlog/spdlog.h>
 #include <cstdint>
-
-typedef boost::interprocess::message_queue_t<usvfs::shared::VoidPointerT> message_queue_interop;
+#include <spdlog/spdlog.h>
 
 namespace spdlog {
 namespace sinks {
 class shm_sink : public sink {
-  message_queue_interop m_LogQueue;
+    std::atomic<int> m_DroppedMessages;
 
-  std::atomic<int> m_DroppedMessages;
-
-public:
-  shm_sink(const char *queueName);
-  virtual void log(const details::log_msg &msg) override;
-  void output(level::level_enum lev, const std::string &message);
-  virtual void flush() override;
+  public:
+    shm_sink(const char* queueName);
+    virtual void log(const details::log_msg& msg) override;
+    void output(level::level_enum lev, const std::string& message);
+    virtual void flush() override;
 };
-}
-}
-
+} // namespace sinks
+} // namespace spdlog
 
 class SHMLogger {
+  public:
+    static const size_t MESSAGE_COUNT = 1024;
+    static const size_t MESSAGE_SIZE = 512;
 
-public:
-  static const size_t MESSAGE_COUNT = 1024;
-  static const size_t MESSAGE_SIZE  = 512;
+  public:
+    static SHMLogger& create(const char* instanceName);
+    static SHMLogger& open(const char* instanceName);
+    static void free();
 
-public:
-  static SHMLogger &create(const char *instanceName);
-  static SHMLogger &open(const char *instanceName);
-  static void free();
+    static bool isInstantiated() { return s_Instance != nullptr; }
 
-  static bool isInstantiated() {
-    return s_Instance != nullptr;
-  }
-
-  static inline SHMLogger &instance() {
-    if (s_Instance == nullptr) {
-      throw std::runtime_error("shm logger not instantiated");
+    static inline SHMLogger& instance() {
+        if (s_Instance == nullptr) {
+            throw std::runtime_error("shm logger not instantiated");
+        }
+        return *s_Instance;
     }
-    return *s_Instance;
-  }
 
-  void log(LogLevel logLevel, const std::string &message);
+    void log(LogLevel logLevel, const std::string& message);
 
-  bool tryGet(char *buffer, size_t bufferSize);
-  void get(char *buffer, size_t bufferSize);
+    bool tryGet(char* buffer, size_t bufferSize);
+    void get(char* buffer, size_t bufferSize);
 
-private:
-  static struct owner_t {
-  } owner;
-  static struct client_t {
-  } client;
+  private:
+    static struct owner_t {
+    } owner;
+    static struct client_t {
+    } client;
 
-private:
-  SHMLogger(owner_t, const std::string &instanceName);
-  SHMLogger(client_t, const std::string &instanceName);
+  private:
+    SHMLogger(owner_t, const std::string& instanceName);
+    SHMLogger(client_t, const std::string& instanceName);
 
-  // not implemented
-  SHMLogger(const SHMLogger &reference);
-  SHMLogger &operator=(const SHMLogger &reference);
+    // not implemented
+    SHMLogger(const SHMLogger& reference);
+    SHMLogger& operator=(const SHMLogger& reference);
 
-  ~SHMLogger();
+    ~SHMLogger();
 
-private:
-  static SHMLogger *s_Instance;
+  private:
+    static SHMLogger* s_Instance;
 
-  message_queue_interop m_LogQueue;
+    std::string m_SHMName;
+    std::string m_LockName;
+    std::string m_QueueName;
 
-  std::string m_SHMName;
-  std::string m_LockName;
-  std::string m_QueueName;
-
-  std::atomic<int> m_DroppedMessages;
+    std::atomic<int> m_DroppedMessages;
 };
--- src/usvfs_shared/src/shmlogger.cpp
+++ src/usvfs_shared/src/shmlogger.cpp
@@ -18,179 +18,161 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with usvfs. If not, see <http://www.gnu.org/licenses/>.
 */
-#pragma warning(disable : 4714)
-#pragma warning(disable : 4503)
-#pragma warning(push, 3)
 #include "usvfs_shared/shmlogger.h"
-#include <boost/interprocess/ipc/message_queue.hpp>
-#include <boost/format.hpp>
-#include <boost/algorithm/string.hpp>
-#include <limits>
 #include <algorithm>
-#pragma warning(pop)
+#include <iterator>
+#include <limits>
+#include <sstream>
+#include <string>
+#include <vector>
 
-#pragma warning(disable : 4996)
+template <typename Out>
+void split(const std::string& s, char delim, Out result) {
+    std::stringstream ss;
+    ss.str(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        *(result++) = item;
+    }
+}
 
-using namespace boost::interprocess;
+std::vector<std::string> split(const std::string& s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems));
+    return elems;
+}
 
-SHMLogger *SHMLogger::s_Instance = nullptr;
+SHMLogger* SHMLogger::s_Instance = nullptr;
 
 SHMLogger::owner_t SHMLogger::owner;
 SHMLogger::client_t SHMLogger::client;
 
-SHMLogger::SHMLogger(owner_t, const std::string &queueName)
-  : m_QueueName(queueName)
-  , m_LogQueue(create_only, queueName.c_str(), MESSAGE_COUNT, MESSAGE_SIZE)
-  , m_DroppedMessages(0)
-{
-  if (s_Instance != nullptr) {
-    throw std::runtime_error("duplicate shm logger instantiation");
-  } else {
-    s_Instance = this;
-  }
-}
-
-SHMLogger::SHMLogger(client_t, const std::string &queueName)
-  : m_QueueName(queueName)
-  , m_LogQueue(open_only, queueName.c_str())
-  , m_DroppedMessages(0)
-{
-  if (s_Instance != nullptr) {
-    throw std::runtime_error("duplicate shm logger instantiation");
-  } else {
-    s_Instance = this;
-  }
-}
-
-SHMLogger::~SHMLogger()
-{
-  s_Instance = nullptr;
-  message_queue_interop::remove(m_QueueName.c_str());
-}
-
-SHMLogger &SHMLogger::create(const char *instanceName)
-{
-  if (s_Instance != nullptr) {
-    throw std::runtime_error("duplicate shm logger instantiation");
-  } else {
-    std::string queueName = std::string("__shm_sink_") + instanceName;
-    message_queue::remove(queueName.c_str());
-    new SHMLogger(owner, queueName);
-    atexit([]() { delete s_Instance; });
-  }
-  return *s_Instance;
-}
-
-SHMLogger &SHMLogger::open(const char *instanceName)
-{
-  if (s_Instance != nullptr) {
-    throw std::runtime_error("duplicate shm logger instantiation");
-  } else {
-    new SHMLogger(client, std::string("__shm_sink_") + instanceName);
-  }
-  return *s_Instance;
-}
-
-void SHMLogger::free()
-{
-  if (s_Instance != nullptr) {
-    SHMLogger *temp = s_Instance;
-    s_Instance      = nullptr;
-    delete temp;
-  }
-}
-
-bool SHMLogger::tryGet(char *buffer, size_t bufferSize)
-{
-  message_queue_interop::size_type receivedSize;
-  unsigned int prio;
-  bool res = m_LogQueue.try_receive(
-      buffer, static_cast<unsigned int>(bufferSize), receivedSize, prio);
-  if (res) {
-    buffer[std::min(bufferSize - 1, static_cast<size_t>(receivedSize))] = '\0';
-  }
-  return res;
-}
-
-void SHMLogger::get(char *buffer, size_t bufferSize)
-{
-  message_queue_interop::size_type receivedSize;
-  unsigned int prio;
-  m_LogQueue.receive(buffer, static_cast<unsigned int>(bufferSize),
-                     receivedSize, prio);
-  buffer[std::min(bufferSize - 1, static_cast<size_t>(receivedSize))] = '\0';
-}
-
-spdlog::sinks::shm_sink::shm_sink(const char *queueName)
-  : m_LogQueue(open_only, (std::string("__shm_sink_") + queueName).c_str())
-{
-}
-
-void spdlog::sinks::shm_sink::flush()
-{
-}
-
-void spdlog::sinks::shm_sink::log(const details::log_msg &msg)
-{
-  int droppedMessages = m_DroppedMessages.load(std::memory_order_relaxed);
-  if (droppedMessages > 0) {
-    std::string dropMessage
-        = fmt::format("{} debug messages dropped", droppedMessages);
-    if (m_LogQueue.try_send(dropMessage.c_str(),
-                            static_cast<unsigned int>(dropMessage.size()), 0)) {
-      m_DroppedMessages.store(0, std::memory_order_relaxed);
+SHMLogger::SHMLogger(owner_t, const std::string& queueName) : m_QueueName(queueName), m_DroppedMessages(0) {
+    if (s_Instance != nullptr) {
+        throw std::runtime_error("duplicate shm logger instantiation");
+    } else {
+        s_Instance = this;
     }
-  }
-
-  std::string message = msg.formatted.str();
-
-  if (message.length() > SHMLogger::MESSAGE_SIZE) {
-    std::vector<std::string> splitVec;
-    boost::split(splitVec, message, boost::is_any_of("\r\n"),
-                 boost::token_compress_on);
-    for (const std::string &line : splitVec) {
-      output(msg.level, line);
-    }
-  } else {
-    output(msg.level, message);
-  }
 }
 
-void spdlog::sinks::shm_sink::output(level::level_enum lev,
-                                     const std::string &message)
-{
-  bool sent = true;
+SHMLogger::SHMLogger(client_t, const std::string& queueName) : m_QueueName(queueName), m_DroppedMessages(0) {
+    if (s_Instance != nullptr) {
+        throw std::runtime_error("duplicate shm logger instantiation");
+    } else {
+        s_Instance = this;
+    }
+}
 
-  // spdlog auto-append line breaks which we don't need. Probably would be
-  // better to not write the breaks to begin with?
-  size_t count = std::min(message.find_last_not_of("\r\n") + 1,
-                          static_cast<size_t>(m_LogQueue.get_max_msg_size()));
+SHMLogger::~SHMLogger() {
+    s_Instance = nullptr;
+    // message_queue_interop::remove(m_QueueName.c_str());
+}
 
-  // depending on the log level, drop less important messages if the receiver
-  // can't keep up
-  switch (lev) {
+SHMLogger& SHMLogger::create(const char* instanceName) {
+    if (s_Instance != nullptr) {
+        throw std::runtime_error("duplicate shm logger instantiation");
+    } else {
+        std::string queueName = std::string("__shm_sink_") + instanceName;
+        // message_queue::remove(queueName.c_str());
+        new SHMLogger(owner, queueName);
+        atexit([]() { delete s_Instance; });
+    }
+    return *s_Instance;
+}
+
+SHMLogger& SHMLogger::open(const char* instanceName) {
+    if (s_Instance != nullptr) {
+        throw std::runtime_error("duplicate shm logger instantiation");
+    } else {
+        new SHMLogger(client, std::string("__shm_sink_") + instanceName);
+    }
+    return *s_Instance;
+}
+
+void SHMLogger::free() {
+    if (s_Instance != nullptr) {
+        SHMLogger* temp = s_Instance;
+        s_Instance = nullptr;
+        delete temp;
+    }
+}
+
+bool SHMLogger::tryGet(char* buffer, size_t bufferSize) {
+    // message_queue_interop::size_type receivedSize;
+    // unsigned int prio;
+    // bool res = m_LogQueue.try_receive(buffer, static_cast<unsigned int>(bufferSize), receivedSize, prio);
+    // if (res) {
+    //    buffer[std::min(bufferSize - 1, static_cast<size_t>(receivedSize))] = '\0';
+    //}
+    // return res;
+    // FIXME: Not implement
+    return false;
+}
+
+void SHMLogger::get(char* buffer, size_t bufferSize) {
+    // message_queue_interop::size_type receivedSize;
+    // unsigned int prio;
+    // m_LogQueue.receive(buffer, static_cast<unsigned int>(bufferSize), receivedSize, prio);
+    // buffer[std::min(bufferSize - 1, static_cast<size_t>(receivedSize))] = '\0';
+    // FIXME: Not implement
+}
+
+spdlog::sinks::shm_sink::shm_sink(const char* queueName) {}
+
+void spdlog::sinks::shm_sink::flush() {}
+
+void spdlog::sinks::shm_sink::log(const details::log_msg& msg) {
+    int droppedMessages = m_DroppedMessages.load(std::memory_order_relaxed);
+    if (droppedMessages > 0) {
+        std::string dropMessage = fmt::format("{} debug messages dropped", droppedMessages);
+        // if (m_LogQueue.try_send(dropMessage.c_str(), static_cast<unsigned int>(dropMessage.size()), 0)) {
+        //    m_DroppedMessages.store(0, std::memory_order_relaxed);
+        //}
+        // FIXME: Not implement
+    }
+
+    std::string message = msg.formatted.str();
+
+    if (message.length() > SHMLogger::MESSAGE_SIZE) {
+        // FIXME: Verify for correctness. \n or \r\n? Was 2nd orginially. -D
+        std::vector<std::string> splitVec = split(message, '\n');
+        for (const std::string& line : splitVec) {
+            output(msg.level, line);
+        }
+    } else {
+        output(msg.level, message);
+    }
+}
+
+void spdlog::sinks::shm_sink::output(level::level_enum lev, const std::string& message) {
+    bool sent = true;
+
+    // spdlog auto-append line breaks which we don't need. Probably would be
+    // better to not write the breaks to begin with?
+    size_t count = std::min(message.find_last_not_of("\r\n") + 1, 1024ULL);
+
+    // depending on the log level, drop less important messages if the receiver
+    // can't keep up
+
+    // FIXME: All this. -D
+    switch (lev) {
     case level::trace:
     case level::debug:
-    case level::notice: {
-      // m_LogQueue.send(message.c_str(), count, 0);
-      sent = m_LogQueue.try_send(message.c_str(),
-                                 static_cast<unsigned int>(count), 0);
+    case level::info: {
+        // sent = m_LogQueue.try_send(message.c_str(), static_cast<unsigned int>(count), 0);
     } break;
-    case level::alert:
+    case level::warn:
     case level::critical:
-    case level::emerg:
     case level::err: {
-      m_LogQueue.send(message.c_str(), static_cast<unsigned int>(count), 0);
+        // m_LogQueue.send(message.c_str(), static_cast<unsigned int>(count), 0);
     } break;
     default: {
-      boost::posix_time::ptime time = microsec_clock::universal_time()
-                                      + boost::posix_time::milliseconds(200);
-      sent = m_LogQueue.timed_send(message.c_str(),
-                                   static_cast<unsigned int>(count), 0, time);
+        // boost::posix_time::ptime time = microsec_clock::universal_time() + boost::posix_time::milliseconds(200);
+        // sent = m_LogQueue.timed_send(message.c_str(), static_cast<unsigned int>(count), 0, time);
     } break;
-  }
+    }
 
-  if (!sent) {
-    m_DroppedMessages.fetch_add(1, std::memory_order_relaxed);
-  }
+    if (!sent) {
+        m_DroppedMessages.fetch_add(1, std::memory_order_relaxed);
+    }
 }
