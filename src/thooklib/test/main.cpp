770bca78cd8d4f5e227d9b604904b479cd9085af
Diana <crazykilla15@gmail.com>
2017-09-14 08:00:23 -0400

    move test_hooks.h crap code

--- src/thooklib/test/main.cpp
+++ src/thooklib/test/main.cpp
@@ -7,6 +7,8 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include <spdlog/spdlog.h>
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
 
 using namespace std;
 using namespace HookLib;
@@ -15,7 +17,34 @@ static const HANDLE MARKERHANDLE = reinterpret_cast<HANDLE>(0x1CC0FFEE);
 static const CHAR INVALID_FILENAME[] = "\\<>/";
 static const WCHAR INVALID_FILENAMEW[] = L"\\<>/";
 
-#include "test_hooks.cpp"
+HANDLE WINAPI THCreateFileA_1(LPCSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode,
+                              LPSECURITY_ATTRIBUTES lpSecurityAttributes, DWORD dwCreationDisposition,
+                              DWORD dwFlagsAndAttributes, HANDLE hTemplateFile) {
+    if (strcmp(lpFileName, INVALID_FILENAME) == 0) {
+        return MARKERHANDLE;
+    } else {
+        HANDLE res = ::CreateFileA(lpFileName, dwDesiredAccess, dwShareMode, lpSecurityAttributes,
+                                   dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
+        return res;
+    }
+}
+
+HANDLE WINAPI THCreateFileW_1(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode,
+                              LPSECURITY_ATTRIBUTES lpSecurityAttributes, DWORD dwCreationDisposition,
+                              DWORD dwFlagsAndAttributes, HANDLE hTemplateFile) {
+    if (wcscmp(lpFileName, INVALID_FILENAMEW) == 0) {
+        EXPECT_EQ(0x42, dwDesiredAccess);
+        EXPECT_EQ(0x43, dwShareMode);
+        EXPECT_EQ(0x44, (int)lpSecurityAttributes);
+        EXPECT_EQ(0x45, dwCreationDisposition);
+        EXPECT_EQ(0x46, dwFlagsAndAttributes);
+        EXPECT_EQ(0x47, (int)hTemplateFile);
+        return MARKERHANDLE;
+    } else {
+        return ::CreateFileW(lpFileName, dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition,
+                             dwFlagsAndAttributes, hTemplateFile);
+    }
+}
 
 static bool stubCalled = false;
 
