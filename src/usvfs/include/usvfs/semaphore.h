39d14609dd6f7d1cebfdb644ac0d38a43b8b023b
Diana <crazykilla15@gmail.com>
2017-09-14 04:33:15 -0400

    Formatting and remove windows.h include in header

--- src/usvfs/include/usvfs/semaphore.h
+++ src/usvfs/include/usvfs/semaphore.h
@@ -1,34 +1,25 @@
 #pragma once
-
-
-#include <windows.h>
-
-
 // based on code by Jeff Preshing
 
 // this is a synchronization class that prefers
 // undefined behaviour over deadlock. It's utterly broken
 // and needs to be replaced in time.
 
+class RecursiveBenaphore {
 
-class RecursiveBenaphore
-{
+  public:
+    RecursiveBenaphore();
+    ~RecursiveBenaphore();
 
-public:
-  RecursiveBenaphore();
-  ~RecursiveBenaphore();
+    // wait on the semaphore. after timeout this will check if the current owner
+    // thread is still alive and steal the semaphore if it isn't. Otherwise this
+    // will continue to wait.
+    void wait(unsigned long timeout = 0xFFFFFFFF);
+    void signal();
 
-  // wait on the semaphore. after timeout this will check if the current owner
-  // thread is still alive and steal the semaphore if it isn't. Otherwise this
-  // will continue to wait.
-  void wait(DWORD timeout = INFINITE);
-  void signal();
-
-private:
-
-  LONG m_Counter;
-  DWORD m_OwnerId;
-  int m_Recursion;
-  HANDLE m_Semaphore;
-
+  private:
+    long m_Counter;
+    unsigned long m_OwnerId;
+    int m_Recursion;
+    void* m_Semaphore;
 };
