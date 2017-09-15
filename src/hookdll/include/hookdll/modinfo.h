6750fd38f88a0736f8fe3fe4a5409ab4ddd158b5
Diana <crazykilla15@gmail.com>
2017-09-14 20:14:21 -0400

    Fix includes

--- src/hookdll/include/hookdll/modinfo.h
+++ src/hookdll/include/hookdll/modinfo.h
@@ -17,11 +17,9 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
-
 #pragma once
-
-#include "logger.h"
-#include <directoryentry.h>
+#include "hookdll/logger.h"
+#include <MO/shared/directoryentry.h>
 #include <list>
 #include <map>
 #include <set>
