daec2997ecaac37fd98938fa8ca7336cedeefd9f
Diana <crazykilla15@gmail.com>
2017-09-14 22:35:02 -0400

    Fix bug cus of third party windows.h includes

--- src/hookdll/src/dllmain.cpp
+++ src/hookdll/src/dllmain.cpp
@@ -19,6 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 // dllmain.cpp : Defines the entry point for the DLL application.
+#define NOMINMAX
 #include "hookdll/dllmain.h"
 #include "MO/Shared/appconfig.h"
 #include "MO/Shared/directoryentry.h"
