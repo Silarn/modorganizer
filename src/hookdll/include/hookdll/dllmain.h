f7ee9913aaae0edc9749b1eb2d4bf6860635f559
Diana <crazykilla15@gmail.com>
2017-09-18 08:49:30 -0400

    Fix Hook.dll

--- src/hookdll/include/hookdll/dllmain.h
+++ src/hookdll/include/hookdll/dllmain.h
@@ -18,6 +18,8 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 #pragma once
+#define COMMON_DLL
+#define COMMON_DLL_BUILD
 #include <common/dllmain.h>
 
 extern "C" DLLEXPORT bool __cdecl Init(int logLevel, const wchar_t* profileName);
--- src/hookdll/src/logger.cpp
+++ src/hookdll/src/logger.cpp
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <tchar.h>
 
 using namespace MOShared;
 
