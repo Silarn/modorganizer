22243a8700b760d3d1c611c2cccc7184ea550bc4
Diana <crazykilla15@gmail.com>
2017-09-18 02:15:24 -0400

    Define NOMINMAX To prevent errors.

--- src/MO/src/logbuffer.cpp
+++ src/MO/src/logbuffer.cpp
@@ -16,7 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Mod Organizer.  If not, see <http://www.gnu.org/licenses/>.
 */
-
+#define NOMINMAX
 #include "MO/logbuffer.h"
 #include <QDateTime>
 #include <QFile>
