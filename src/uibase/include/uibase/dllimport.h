dd322d851a34e1c79311b0b6530b2ee2c3375db9
Diana <crazykilla15@gmail.com>
2017-09-21 18:33:08 -0400

    Improve CMake files.
    Made them more consistent, reliable, and sane.

--- src/uibase/include/uibase/dllimport.h
+++ src/uibase/include/uibase/dllimport.h
@@ -18,6 +18,7 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 #pragma once
+#define COMMON_DLL
 #include <common/dllmain.h>
 
 #define QDLLEXPORT DLLEXPORT
