24024755b24e240c7f396b7b6e815499904534a5
Diana <crazykilla15@gmail.com>
2017-10-01 03:09:34 -0400

    Remove appconfig cruft and simplify appconfig code.
    It seems, at first glance, the goal was to help compile times in the exceedingly rare and unlikely event that these constants would be changed.
    
    A failed attempt, at that, since changing them would mean changing the appconfig.inc file which means the header needs to be compiled anyway, preventing any benefit from using the cpp file.
    
    In fact, it would also hurt optimization since, without Link Time Optimization it these can't be optimized away.
    
    I've simplifed things and removed the appconfig.inc and appconfig.cpp files and put it all ine the header.

--- src/MO/include/MO/shared/appconfig.h
+++ src/MO/include/MO/shared/appconfig.h
@@ -16,24 +16,36 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Mod Organizer.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-#ifndef APPCONFIG_H
-#define APPCONFIG_H
-
+#pragma once
 #include <string>
 
 namespace AppConfig {
 
-#define PARWSTRING wstring
-#define APPPARAM(partype, parid, value) partype parid();
-#include "MO/shared/appconfig.inc"
+#define APPPARAM(partype, parid, value)                                                                                \
+    partype parid() { return value; }
 
-namespace MOShared {
-#undef PARWSTRING
+APPPARAM(std::wstring, translationPrefix, L"organizer")
+APPPARAM(std::wstring, pluginPath, L"plugins")
+APPPARAM(std::wstring, profilesPath, L"profiles")
+APPPARAM(std::wstring, modsPath, L"mods")
+APPPARAM(std::wstring, downloadPath, L"downloads")
+APPPARAM(std::wstring, overwritePath, L"overwrite")
+APPPARAM(std::wstring, stylesheetsPath, L"stylesheets")
+APPPARAM(std::wstring, cachePath, L"webcache")
+APPPARAM(std::wstring, tutorialsPath, L"tutorials")
+APPPARAM(std::wstring, logPath, L"logs")
+APPPARAM(std::wstring, profileTweakIni, L"profile_tweaks.ini")
+APPPARAM(std::wstring, logFileName, L"ModOrganizer.log")
+APPPARAM(std::wstring, iniFileName, L"ModOrganizer.ini")
+APPPARAM(std::wstring, proxyDLLTarget, L"steam_api.dll")
+APPPARAM(std::wstring, proxyDLLOrig,
+         L"steam_api_orig.dll") // needs to be identical to the value used in proxydll-project
+APPPARAM(std::wstring, proxyDLLSource, L"proxy.dll")
+APPPARAM(const wchar_t*, localSavePlaceholder, L"__MOProfileSave__\\")
+
+APPPARAM(std::wstring, firstStepsTutorial, L"tutorial_firststeps_main.js")
+APPPARAM(int, numLogFiles, 5) // number of log files to keep
+
 #undef APPPARAM
 
-} // namespace MOShared
-
 } // namespace AppConfig
-
-#endif // APPCONFIG_H
